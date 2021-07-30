457f94b3c7443ed003029ed0a2b7f8ed471a9bff
pratyush-kr <pratyush151198@gmail.com>
2021-07-30 11:32:46 +0530

    Fixed

--- Lab4 29-07-2021/merge-sort.cpp
+++ Lab4 29-07-2021/merge-sort.cpp
@@ -15,35 +15,36 @@ int randomNumber()
 void merge(std::vector<int> &arr, int l, int m , int r)
 {
     int n1, n2;
-    n1 = m - l + 1;
-    n2 = r - m;
-    std::vector<int> arr1, arr2;
-    for(int i=0; i<n1; i++)
+    n1 = m;
+    n2 = r;
+    std::vector<int> arr1, arr2, array;
+    for(int i=l; i<n1; i++)
         arr1.push_back(arr[i]);
     for(int i=n1; i<n2; i++)
         arr2.push_back(arr[i]);
-    int i=0, j=0, z=0;
-    while(i < n1 && j < n2)
+    int i=0, j=0, z=l;
+    while(i < arr1.size() && j < arr2.size())
     {
         if(arr1[i] < arr2[j])
             arr[z++] = arr1[i++];
         else
             arr[z++] = arr2[j++];
     }
-    while(i<n1)
+    while(i<arr1.size())
         arr[z++] = arr1[i++];
-    while(j<n2)
+    while(j<arr2.size())
         arr[z++] = arr2[j++];
 }
 
 void merge_sort(std::vector<int> &arr, int l, int r)
 {
     func_call++;
-    if(l <= r)
+    if(l < r)
     {
         int mid = (l+r)/2;
         merge_sort(arr, l, mid);
         merge_sort(arr, mid+1, r);
+        std::cout<<"It works\n";
         merge(arr, l, mid, r);
     }
 }
@@ -68,7 +69,7 @@ int main()
         arrays.push_back(*arr);
     }
     int key, time_taken;
-    std::ofstream file("Bs_data.csv");
+    std::ofstream file("MS_data.csv");
     file<<"n,timetaken\n";
     for(int i=0; i<arrays.size(); i++)
     {
