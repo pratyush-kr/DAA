6172d5a97975c2098153e2329e3f2d56aae17533
pratyush-kr <pratyush151198@gmail.com>
2021-10-01 11:33:02 +0530

    Activity Selection

--- Lab7 26-08-2021/activity-selection.cpp
+++ Lab7 26-08-2021/activity-selection.cpp
@@ -1,27 +1,48 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
- 
-void printMaxActivities(int s[], int f[], int n)
+
+struct Activity
 {
-    int i, j;
-    cout <<"Following activities are selected "<< endl;
-    i = 0;
-    cout <<" "<< i;
-    for (j = 1; j < n; j++)
-    {
-      if (s[j] >= f[i])
-      {
-          cout <<" " << j;
-          i = j;
-      }
-    }
-}
+    char ID;
+    unsigned int start;
+    unsigned int end;
+};
 
 int main()
 {
-    int s[] =  {1, 3, 0, 5, 8, 5};
-    int f[] =  {2, 4, 6, 7, 9, 9};
-    int n = sizeof(s)/sizeof(s[0]);
-    printMaxActivities(s, f, n);
+    vector<Activity*> arr;
+    int n;
+    cout<<"n: ";
+    cin>>n;
+    Activity *ptr;
+    printf("ID  Start   End\n");
+    for(int i=0; i<n; i++)
+    {
+        ptr = new Activity;
+        cin>>ptr->ID>>ptr->start>>ptr->end;
+        arr.push_back(ptr);
+    }
+    sort(arr.begin(), arr.end(), [](Activity *a, Activity *b){return (a->end < b->end);});
+    printf("Sorted Activities\n");
+    printf("ID  Start   End\n");
+    for(auto x : arr)
+        cout<<x->ID<<"  "<<x->start<<"  "<<x->end<<'\n';
+    int take = 1;
+    int end = arr[0]->end;
+    printf("Activity Selected:\n");
+    printf("ID  Start   End\n");
+    printf("%c  %d  %d\n", arr[0]->ID, arr[0]->start, arr[0]->end);
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i]->start >= end)
+        {
+            printf("%c  %d  %d\n", arr[i]->ID, arr[i]->start, arr[i]->end);
+            take++;
+            end = arr[i]->end;
+        }
+    }
+    cout<<take<<'\n';
     return 0;
 }
