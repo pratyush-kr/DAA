abf6695e3a9f18556c619ea05101293f878f5acd
pratyush-kr <pratyush151198@gmail.com>
2021-10-01 23:35:27 +0530

    LCS

--- Lab8 23-09-2021/LCS.cpp
+++ Lab8 23-09-2021/LCS.cpp
@@ -3,60 +3,50 @@
 
 using namespace std;
 
-struct LCS
+struct Strings
 {
     char *A;
     char *B;
     int m;
     int n;
     int **memo;
-    LCS(string A, string B)
+    Strings(string A, string B)
     {
         this->A = &A[0];
         this->B = &B[0];
         m = A.size();
         n = B.size();
         memo = new int*[m];
-        for(int i=0; i<n; i++)
+        for(int i=0; i<m; i++)
             memo[i] = new int[n];
         for(int i=0; i<m; i++)
             for(int j=0; j<n; j++)
-                memo[i][j] = 0;
+                memo[i][j] = -1;
     }
+    int lcs(int , int);
 };
 
-int lcs_len(LCS x, int m, int n)
+int Strings::lcs(int i, int j)
 {
-    if(m == 0 || n == 0)
-        return 0;
-    if(x.memo[m-1][n-1] != 0)
-        return x.memo[m-1][n-1];
-    if(x.A[m-1] == x.B[n-1])
-    {
-        x.memo[m-1][n-1] = 1 + lcs_len(x, m-1, n-1);
-        return x.memo[m-1][n-1];
-    }
-    else
-    {
-        x.memo[m-1][n-1] = max(lcs_len(x, m-1, n), lcs_len(x, m, n-1));
-        return x.memo[m-1][n-1];
-    }
-    return 0;
+
 }
 
 int main()
 {
     string A, B;
-    printf("A: ");
-    cin>>A;
-    printf("B: ");
-    cin>>B;
-    LCS x(A, B);
-    lcs_len(x, x.m, x.n);
+    A = "stone";
+    B = "longest";
+    printf("A: %s\n", &A[0]);
+    printf("B: %s\n", &B[0]);
+    Strings x(A, B);
+    x.lcs(x.m, x.n);
     for(int i=0; i<x.m; i++)
     {
         for(int j=0; j<x.n; j++)
-            cout<<x.memo[i][j]<<" ";
+        {
+            int l = x.memo[i][j];
+            cout<<((l == -1)?l+1:l)<<" ";
+        }
         cout<<'\n';
     }
     return 0;
