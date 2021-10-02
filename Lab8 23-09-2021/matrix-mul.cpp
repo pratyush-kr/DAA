2de96b761de67d55eaf05bded5100c7085e6fb65
pratyush-kr <pratyush151198@gmail.com>
2021-10-02 14:42:51 +0530

    Matrix-Chain-Order

--- Lab8 23-09-2021/matrix-mul.cpp
+++ Lab8 23-09-2021/matrix-mul.cpp
@@ -5,17 +5,28 @@ using namespace std;
 
 struct DP
 {
-    int **memo;
+    int **m;
+    int **s;
     int n;
     DP(int arr_len)
     {
         n = arr_len;
-        memo = new int*[n];
+        m = new int*[n];
+        s = new int*[n];
         for(int i=0; i<n; i++)
-            memo[i] = new int[n];
+        {
+            m[i] = new int[n];
+            s[i] = new int[n];
+        }
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+            {
+                m[i][j] = -1;
+                s[i][j] = 0;
+            }
         cout<<"Constructor Sucess\n";
     }
-};
+}*dp;
  
 int MatrixChainOrder(vector<int> M, int i, int j)
 {
@@ -24,12 +35,17 @@ int MatrixChainOrder(vector<int> M, int i, int j)
     int k;
     int min = INT32_MAX;
     int count;
+    if(dp->m[i][j] != -1)
+    {
+        return dp->m[i][j];
+    }
     for(int k=i; k<j; k++)
     {
         count = MatrixChainOrder(M, i, k) + 
                 MatrixChainOrder(M, k+1, j) +
                 M[i-1]*M[k]*M[j];
         min = (count<min)? count:min;
+        dp->m[i][j] = min;
     }
     return min;
 }
@@ -38,7 +54,8 @@ int main()
 {
     vector<int> M;
     int arr[] = {10, 20, 30, 40, 50, 60};
-    int n = 6;
+    int n = sizeof(arr)/sizeof(*arr);
+    dp = new DP(n);
     for(int i=0; i<n; i++)
         M.push_back(arr[i]);
     cout<<MatrixChainOrder(M, 1, n-1)<<'\n';
