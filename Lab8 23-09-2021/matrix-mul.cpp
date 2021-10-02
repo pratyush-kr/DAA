3d0810e9156b8b7027384aca56cf3cac6b47549d
pratyush-kr <pratyush151198@gmail.com>
2021-10-02 15:29:01 +0530

    Matrix-Multiplication

--- Lab8 23-09-2021/matrix-mul.cpp
+++ Lab8 23-09-2021/matrix-mul.cpp
@@ -33,7 +33,7 @@ int MatrixChainOrder(vector<int> M, int i, int j)
     if(i == j)
         return 0;
     int k;
-    int min = INT32_MAX;
+    int min = INT32_MAX, min_k;
     int count;
     if(dp->m[i][j] != -1)
         return dp->m[i][j];
@@ -42,9 +42,13 @@ int MatrixChainOrder(vector<int> M, int i, int j)
         count = MatrixChainOrder(M, i, k) + 
                 MatrixChainOrder(M, k+1, j) +
                 M[i-1]*M[k]*M[j];
-        min = (count<min)? count:min;
+        if(count<min)
+        {
+            min = count;
+            min_k = k;
+        }
         dp->m[i][j] = min;
-        dp->s[i][j] = k;
+        dp->s[i][j] = min_k;
     }
     return min;
 }
@@ -52,16 +56,27 @@ int MatrixChainOrder(vector<int> M, int i, int j)
 int main()
 {
     vector<int> M;
-    int arr[] = {3, 2, 4, 2, 5};
+    int arr[] = {10, 20, 30, 40, 50, 60};
     int n = sizeof(arr)/sizeof(*arr);
     dp = new DP(n);
     for(int i=0; i<n; i++)
         M.push_back(arr[i]);
-    cout<<MatrixChainOrder(M, 1, n-1)<<'\n';
+    cout<<"Min Operations: "<<MatrixChainOrder(M, 1, n-1)<<'\n';
+    cout<<"S-Table\n";
     for(int i=1; i<n; i++)
     {
         for(int j=1; j<n; j++)
             cout<<dp->s[i][j]<<" ";
+        cout<<'\n';
+    }
+    for(int i=n-1; i>=2; i--)
+    {
+        cout<<"(";
+        for(int j=1; j<=dp->s[1][i]; j++)
+            printf("%c", 64+j);
+        cout<<")";
+        for(int j=dp->s[1][i]+1; j<n; j++)
+            printf("%c", 64+j);
         cout<<'\n';
     }
 }
