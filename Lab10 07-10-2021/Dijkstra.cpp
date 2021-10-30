f4eba1526ce67015895deb6dd5ee884b3d759bb8
pratyush-kr <pratyush151198@gmail.com>
2021-10-30 10:06:32 +0530

    Lab10

--- Lab10 07-10-2021/Dijkstra.cpp
+++ Lab10 07-10-2021/Dijkstra.cpp
@@ -32,20 +32,41 @@ struct Graph
         else if(nodes[a] == NULL)
             nodes[a] = new Node(a);
         else if(nodes[b] == NULL)
-            nodes[b] = NULL;
+            nodes[b] = new Node(b);
         nodes[a]->adjList.push_back(nodes[b]);
         nodes[b]->adjList.push_back(nodes[a]);
         weights[{a, b}] = w;
         weights[{b, a}] = w;
     }
+    void dfs(int s);
 };
 
 int main()
 {
     int edges, vertices;
+    int a, b, w;
     printf("Edges: ");
     cin>>edges;
     printf("Vertices: ");
     cin>>vertices;
+    Graph g(vertices, edges);
+    printf("a b w\n");
+    for(int i=0; i<edges; i++)
+    {
+        cin>>a>>b;
+        g.addEdge(a, b);
+    }
+    g.dfs(1);
+    cout<<'\n';
     return 0;
+}
+
+void Graph::dfs(int s)
+{
+    static map<int, bool> visited;
+    visited[s] = true;
+    cout<<nodes[s]->id<<" ";
+    for(auto x : nodes[s]->adjList)
+        if(!visited[x->id])
+            dfs(x->id);
 }
