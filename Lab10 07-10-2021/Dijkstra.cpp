3b1995225b7b500264fa77bd91b0620faa775a9f
pratyush-kr <pratyush151198@gmail.com>
2021-10-30 09:15:21 +0530

    Lab10

--- Lab10 07-10-2021/Dijkstra.cpp
+++ Lab10 07-10-2021/Dijkstra.cpp
@@ -1,23 +1,51 @@
 #include<iostream>
 #include<vector>
+#include<map>
 
 using namespace std;
 
 struct Node
 {
     int id;
-    vector<Node*> adj;
+    vector<Node*> adjList;
+    Node(int id){this->id = id;}
 };
 
 struct Graph
 {
     int vertices;
     int edges;
-    vector<Node*> nodes;
-    
+    map<int, Node*> nodes;
+    map<pair<int, int>, int> weights;
+    Graph(int vertices, int edges)
+    {
+        this->vertices = vertices;
+        this->edges = edges;
+    }
+    void addEdge(int a, int b, int w = 0)
+    {
+        if(nodes[a] == NULL && nodes[b] == NULL)
+        {
+            nodes[a] = new Node(a);
+            nodes[b] = new Node(b);
+        }
+        else if(nodes[a] == NULL)
+            nodes[a] = new Node(a);
+        else if(nodes[b] == NULL)
+            nodes[b] = NULL;
+        nodes[a]->adjList.push_back(nodes[b]);
+        nodes[b]->adjList.push_back(nodes[a]);
+        weights[{a, b}] = w;
+        weights[{b, a}] = w;
+    }
 };
 
 int main()
 {
+    int edges, vertices;
+    printf("Edges: ");
+    cin>>edges;
+    printf("Vertices: ");
+    cin>>vertices;
     return 0;
 }
